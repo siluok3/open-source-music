981cc79fe6cc54e3edb857a14cbe0e575153ec81
Kiriakos Papachristou <koulispapas@gmail.com>
2019-05-29 17:33:29 +0200

    Basic setup for client microservice, Gin Gonic in Containers

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//Set the router as the default one
+	router := gin.Default()
+
+	//Serve the frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
+
+	//Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "All good my friend",
+			})
+		})
+	}
+
+	router.Run(":6969")
+}
